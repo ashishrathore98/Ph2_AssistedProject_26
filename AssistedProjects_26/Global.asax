9187a4835aeb54fb812db8b0f9120ad7668c9e30
Ashish Rathore <ashrathore98@gmail.com>
2023-09-05 02:30:49 +0530

    first commit

---
+++ AssistedProjects_26/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AssistedProjects_26.MvcApplication" Language="C#" %>
---
+++ AssistedProjects_26/Models/IService1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssistedProjects_26.Models
+{
+    public class IService1
+    {
+    }
+}
